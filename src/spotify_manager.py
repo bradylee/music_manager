c7bc9b4b0487407f6962224f60c4aa704a8d1768
Brady Lee <failure.online@gmail.com>
2022-10-08 23:56:23 -0700

    Move request for playlist items to a function

--- src/spotify_manager.py
+++ src/spotify_manager.py
@@ -15,18 +15,27 @@ def get_spotify_request_headers(token):
     return headers
 
 
-if __name__ == "__main__":
-    # Get command line arguments.
-    token = sys.argv[1]
-    playlist_id = sys.argv[2]
-
+def get_spotify_playlist_items(token, playlist_id):
+    """
+    Request items from a Spotify playlist.
+    """
     # API endpoint to get tracks from a playlist.
     endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
     # Execute the GET request.
     headers = get_spotify_request_headers(token)
     response = requests.get(endpoint, headers=headers)
-    print(response.status_code)
 
-    if response.status_code == 200:
-        print(json.dumps(response.json(), indent=4))
+    if response.status_code != 200:
+        return None
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    # Get command line arguments.
+    token = sys.argv[1]
+    playlist_id = sys.argv[2]
+
+    data = get_spotify_playlist_items(token, playlist_id)
+    print(json.dumps(data, indent=4))
--- tests/test_spotify_manager.py
+++ tests/test_spotify_manager.py
@@ -3,16 +3,40 @@ from src.spotify_manager import *
 
 def test_get_spotify_request_headers():
     """
-    Test `get_spotify_request_headers` by calling with different token strings and verifying the constructed value.
+    Test `get_spotify_request_headers` by calling with different token strings and checking the
+    constructed value.
     """
     # Test a sample token.
-    token = 'sample'
+    token = "sample"
     headers = get_spotify_request_headers(token)
     assert headers["Accept"] == "application/json"
     assert headers["Content-Type"] == "application/json"
     assert headers["Authorization"] == "Bearer sample"
 
     # Test another sample.
-    token = 'another_sample'
+    token = "another_sample"
     headers = get_spotify_request_headers(token)
     assert headers["Authorization"] == "Bearer another_sample"
+
+
+def test_get_spotify_playlist_items(requests_mock):
+    """
+    Test `get_spotify_playlist_items` by mocking the request and checking the response.
+    """
+    # Set arbitrary values since the request is mocked.
+    token = "sample"
+    playlist_id = "example"
+    endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    response_data = {"hello": "world"}
+
+    # Test JSON data is returned on a good response.
+    status_code = 200
+    requests_mock.get(endpoint, json=response_data, status_code=status_code)
+    data = get_spotify_playlist_items(token, playlist_id)
+    assert data == response_data
+
+    # Test None is returned on a bad response.
+    status_code = 400
+    requests_mock.get(endpoint, json=response_data, status_code=status_code)
+    data = get_spotify_playlist_items(token, playlist_id)
+    assert data == None
