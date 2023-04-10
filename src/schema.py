19e516c0885b1aa52e5638966e37ab8f39deba81
Brady Lee <failure.online@gmail.com>
2023-04-09 18:13:15 -0700

    schema 1.2.0

--- src/schema.py
+++ src/schema.py
@@ -28,3 +28,12 @@ schemas["1.1.0"]["tracks"].update({
     # The number of times a track has been rated.
     "num_times_rated": "int NOT NULL DEFAULT 0 CHECK (num_times_rated >= 0)",
 })
+
+# Add is_fetched column to albums.
+schemas["1.2.0"] = copy.deepcopy(schemas["1.1.0"])
+schemas["1.2.0"]["albums"].update({
+    # Set to True when all tracks from the album have been set. This is to prevent
+    # needlessly fetching an album multiple times since albums will not have tracks
+    # modified after the intial release.
+    "is_fetched": "boolean NOT NULL DEFAULT 0",
+})
